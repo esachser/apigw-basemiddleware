b0a559063d7a9babed122526058eface35dfae2d
Eduardo Sachser <eduardosachser@MacBook-Pro-de-Eduardo.local>
2020-04-14 23:29:31 -0300

    First commit

---
+++ BaseMiddleware.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace BaseMiddleware
+{
+    public delegate Task MiddlewareDelegate(HttpContext context, MiddlewareDelegate next);
+
+    public interface IBaseMiddleware
+    {
+        Task Invoke(HttpContext context, MiddlewareDelegate next);
+    }
+}
